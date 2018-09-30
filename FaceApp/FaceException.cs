5f72478712d18762637a95422b2ac5e62e031361
Quahu <quahuu@gmail.com>
2018-09-30 21:30:48 +0200

    ConfigureAwait and minor code improvements.

--- FaceApp/FaceException.cs
+++ FaceApp/FaceException.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FaceApp
 {
     public class FaceException : Exception
     {
-        public ExceptionType Type { get; set; }
+        public ExceptionType Type { get; }
 
-        public FaceException(ExceptionType type, string message) : base(message)
-        {
-            this.Type = type;
-        }
+        public FaceException(ExceptionType type, string message) : base(message) 
+            => Type = type;
 
-        public override string ToString()
-        {
-            return $"{Type}: {Message}";
-        }
+        public override string ToString() 
+            => $"{Type}: {Message}";
     }
 }
